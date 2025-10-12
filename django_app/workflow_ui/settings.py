101653004547d790237bc9d025d89e86c98a0442
ANU-2524 <anusoni25.2006@gmail.com>
2025-10-12 20:57:34 +0530

    Deployment pending

--- django_app/workflow_ui/settings.py
+++ django_app/workflow_ui/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -42,6 +43,10 @@ INSTALLED_APPS = [
     'channels',
 ]
 ASGI_APPLICATION = "workflow_ui.asgi.application"
+
+DATABASES = {
+    'default': dj_database_url.config(conn_max_age=600)
+}
 
 CHANNEL_LAYERS = {
     'default': {
